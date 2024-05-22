81f1818670b12b81b1fb0d12248765986ec88f15
Rafiul Islam <rafi.cse.ru@gmail.com>
2024-05-22 15:08:09 +0600

    Display genrers

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,25 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data: geners } = useGenres();
   return (
-    <ul>
-      {geners.map(({ id, name }) => (
-        <li key={id}>{name}</li>
+    <List>
+      {geners.map(({ id, name, image_background }) => (
+        <ListItem key={id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              objectFit="cover"
+              borderRadius={8}
+              src={getCroppedImageUrl(image_background)}
+            />
+            <Text fontSize="lg">{name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
