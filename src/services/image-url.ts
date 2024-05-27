5cc7a76d2fe3e4ca7e0a56a1b3156cc10f0a8b45
Rafiul Islam <rafi.cse.ru@gmail.com>
2024-05-27 22:17:01 +0600

    Handle game with no image.

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import defaultGameImage from "../assets/default-game-image.jpg";
+
 function getCroppedImageUrl(imageURL: string) {
-  if (!imageURL) return "";
+  if (!imageURL) return defaultGameImage;
   const updatedUrl = imageURL.replace("/media/", "/media/crop/600/400/");
   return updatedUrl;
 }
