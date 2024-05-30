45ea83bfc352b776c964d0bdd9391b0a58be17e8
Rafiul Islam <rafi.cse.ru@gmail.com>
2024-05-31 01:13:43 +0600

    Put the API key to the .env file.

--- src/services/http.ts
+++ src/services/http.ts
@@ -3,7 +3,7 @@ import axios, { isCancel, AxiosRequestConfig } from "axios";
 const http = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "53876516b386461abfda36a814eb506f",
+    key: import.meta.env.VITE_APP_API_KEY,
   },
 });
 
