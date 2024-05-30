fcdaae03805691a1924bf1e3ed24a4940c596b47
Rafiul Islam <rafi.cse.ru@gmail.com>
2024-05-30 23:13:13 +0600

    Add emojis.

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,13 +3,15 @@ import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
+import Emoji from "./Emoji";
 
 interface Props {
   game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
-  const { name, background_image, parent_platforms, metacritic } = game;
+  const { name, background_image, parent_platforms, metacritic, rating_top } =
+    game;
 
   return (
     <Card>
@@ -23,7 +25,10 @@ const GameCard = ({ game }: Props) => {
           <PlatformIconList platforms={parent_platforms} />
           <CriticScore score={metacritic} />
         </HStack>
-        <Heading fontSize="xl">{name}</Heading>
+        <Heading fontSize="xl">
+          {name}
+          <Emoji rating={rating_top} />
+        </Heading>
       </CardBody>
     </Card>
   );
