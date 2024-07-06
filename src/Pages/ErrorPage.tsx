5e728ed131f99603bc604b5c0e0968bdcfd92afb
Rafiul Islam <rafi.cse.ru@gmail.com>
2024-07-06 11:36:50 +0600

    Handle errors.

---
+++ src/Pages/ErrorPage.tsx
@@ -0,0 +1,23 @@
+import { Container, Heading, Text } from "@chakra-ui/react";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+
+  return (
+    <>
+      <Navbar />
+      <Container maxW="1200px" mt="30px">
+        <Heading fontSize="5xl">Opps</Heading>
+        <Text fontSize="xl">
+          {isRouteErrorResponse(error)
+            ? "The page does not exits."
+            : "Sorry, an unexpacted error occure."}
+        </Text>
+      </Container>
+    </>
+  );
+};
+
+export default ErrorPage;
--- src/router.tsx
+++ src/router.tsx
@@ -2,10 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import GameDetails from "./components/GameDetails";
 import Layout from "./Pages/Layout";
 import HomePage from "./Pages/HomePage";
+import ErrorPage from "./Pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    errorElement: <ErrorPage />,
     element: <Layout />,
     children: [
       { index: true, element: <HomePage /> },
